15e7f359b5b64cb96c26d72e131c53c1495d7bb1
jun931223 <jun931223@gmail.com>
2022-07-26 05:30:54 +0900

    feat: chart를 이용한 train 데이터 분석

--- Titanic/index.py
+++ Titanic/index.py
@@ -1,6 +1,6 @@
-import pandas as pd
+import pandas as pd #pandas 라이브러리에 대해서 간단하게 정리
 import matplotlib.pyplot as plt
-# import seaborn as sns
+import seaborn as sns
 
 train = pd.read_csv("Titanic/input/train.csv")
 test = pd.read_csv("Titanic/input/test.csv")
@@ -15,16 +15,27 @@ test = pd.read_csv("Titanic/input/test.csv")
   # embarked  | Port of Embarkation C = Cherbourg, Q = Queenstown, S = Southampton | 탑승 항구 정보 
 
 print(train.head(80))
-train.info()
-# train.isnull().sum()
+
+train.shape # train의 로우와 컬럼 수 891,12 
+train.info() # Age / Cabin / Embarked 의 항목이 전체 행의 개수와 맞지 않음 -> 데이터 가공이 필요한 값이 있음
+train.isnull().sum() # 데이터에서 null인 값들을 더한 후 보여줌
+sns.set() # setting seaborn default for plots
+
 def bar_chart(feature):
     survived = train[train['Survived']==1][feature].value_counts()
     dead = train[train['Survived']==0][feature].value_counts()
     df = pd.DataFrame([survived,dead])
     df.index = ['Survived','Dead']
     df.plot(kind='bar',stacked=True, figsize=(10,5))
+    plt.show()
 
 
-bar_chart('Sex')
+bar_chart('Sex') # 성비율에 따른 생존률  남성 < 여성
+bar_chart("Pclass") # 비행기 등급에 따른 생존률 3 < 2 < 1
 
-# sns.set() # setting seaborn default for plots
+# 총 승객수에서 가족과 함께 탄 사람들의 정확한 비율이 있어야 데이터를 해석할 수 있을 것 같음
+bar_chart("SibSp") # 형제, 자매와 함께 탑승했을 경우의 생존률
+bar_chart("Parch") # 가족과 함께 탑승했을 경우의 생존률
+
+bar_chart("Embarked") # 선착장에 따른 생존률
+# 
